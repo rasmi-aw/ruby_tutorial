fa07d722d8329874eb755118542bc5c5ea316623
rasmi-aw <abdelwadoudrasmi@gmail..com>
2022-09-07 15:47:33 +0200

    update

--- code/classes.rb
+++ code/classes.rb
@@ -2,6 +2,12 @@ class Person
   #getter setter
   attr_accessor :name, :age, :profession
 
+
+  @@max_age = 100
+  def self.define
+    puts "a Person is a being created by god to worship him and lives from 0 to #{@@max_age} years old"
+  end
+
   def initialize(fullname, age, profession)
     @name = fullname
     @age = age
@@ -21,3 +27,4 @@ me = Person.new("Abdelwadoud rasmi", 24, "Software ENgineer")
 me.inspect
 me.name = "AbdelWadoud "
 me.inspect
+Person.define
